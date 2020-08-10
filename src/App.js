1fde13d2083955fdd52fb887ff049dac09384f55
Gabriele Polenta <g.polenta@orderfox.com>
2020-08-10 10:44:09 +0200

    Chat background image, package.json update, App css, readme file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import Title from './components/Title/Title';
+import MessagesList from './components/MessagesList/MessagesList';
+import SendMessageForm from './components/SendMessageForm/SendMessageForm';
 
 function App() {
+  const myUsername = "Gabriele Polenta";
+  // const myUsername = "Mr Smith";
+
   return (
     <div className="App">
+      <Title titleText="Doodle Chat"/>
+      <MessagesList
+        myUsername={myUsername}
+      />
+      <SendMessageForm
+        myUsername={myUsername}
+      />
     </div>
   );
 }
+
+App.displayName = 'App';
 
 export default App;
